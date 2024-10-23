9dfd15833e74fadb0f7be0760c1678c3359f091e
BrandfordL <brandford.hoyos@correounivalle.edu.co>
2024-10-22 19:28:43 -0500

    Update practica.py

--- practica.py
+++ practica.py
@@ -1,1 +1,11 @@
 print("Hola Morenito papasote bello")
+edad = int(input("Ingresa tu edad: "))
+
+if 0 < edad <= 12:
+    print("Eres un niño.")
+elif 12 < edad < 18:
+    print("Eres un adolescente.")
+elif 18 <= edad < 50:
+    print("Eres un adulto.")
+else:
+    print("Eres una persona mayor.")
